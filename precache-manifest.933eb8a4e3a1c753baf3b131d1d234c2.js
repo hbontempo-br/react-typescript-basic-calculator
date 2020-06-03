b2bb1d47406bbe47fa58cb32fe9ef52bd26d26bd
Henrique Bontempo <me@hbontempo.dev>
2020-06-03 14:49:42 -0300

    Updates

---
+++ precache-manifest.933eb8a4e3a1c753baf3b131d1d234c2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2102511383ea9b055677d33ecc6ffa17",
+    "url": "/react-typescript-basic-calculator/index.html"
+  },
+  {
+    "revision": "a81134112f7696923d2a",
+    "url": "/react-typescript-basic-calculator/static/css/2.d54bb455.chunk.css"
+  },
+  {
+    "revision": "a81134112f7696923d2a",
+    "url": "/react-typescript-basic-calculator/static/js/2.09959b36.chunk.js"
+  },
+  {
+    "revision": "c346956c1449420dcbe7f2a51cdfc9c4",
+    "url": "/react-typescript-basic-calculator/static/js/2.09959b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "451e398bfed569ce7601",
+    "url": "/react-typescript-basic-calculator/static/js/3.cd3787d0.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/react-typescript-basic-calculator/static/js/3.cd3787d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f0604630e7912285e3",
+    "url": "/react-typescript-basic-calculator/static/js/4.7b5b4d3c.chunk.js"
+  },
+  {
+    "revision": "f4fe75836385330d3549",
+    "url": "/react-typescript-basic-calculator/static/js/5.28f6ede1.chunk.js"
+  },
+  {
+    "revision": "bcb988111a79dabf62fc",
+    "url": "/react-typescript-basic-calculator/static/js/6.65d420ff.chunk.js"
+  },
+  {
+    "revision": "f3e74831e56d3c1683e9",
+    "url": "/react-typescript-basic-calculator/static/js/7.285802da.chunk.js"
+  },
+  {
+    "revision": "dc01933a669adb9543e4",
+    "url": "/react-typescript-basic-calculator/static/js/8.64050fef.chunk.js"
+  },
+  {
+    "revision": "481924330d33abbe47f1",
+    "url": "/react-typescript-basic-calculator/static/js/9.729cd2a7.chunk.js"
+  },
+  {
+    "revision": "82ebcae5423292853b2f",
+    "url": "/react-typescript-basic-calculator/static/js/main.f3e404d4.chunk.js"
+  },
+  {
+    "revision": "330a639517af67bae50a",
+    "url": "/react-typescript-basic-calculator/static/js/runtime-main.6b261f51.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-typescript-basic-calculator/precache-manifest.933eb8a4e3a1c753baf3b131d1d234c2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-typescript-basic-calculator/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
